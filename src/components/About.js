59068c5576c48388e31b2aae254f8199406320ba
Khazdev <xazanoff17@gmail.com>
2024-02-18 15:44:03 +0300

    320 landing

--- src/components/About.js
+++ src/components/About.js
@@ -6,10 +6,18 @@ function About() {
     <section className="about">
       <h3 className="about__header">О проекте</h3>
       <div className="about__grid-container about__grid-container_description">
-        <h4 className="about__description-title">Дипломный проект включал 5 этапов</h4>
-        <h4 className="about__description-title">На выполнение диплома ушло 5 недель</h4>
-        <p className="about__description">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и&nbsp;финальные доработки.</p>
-        <p className="about__description">У&nbsp;каждого этапа был мягкий и&nbsp;жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        <div className="about__description-column">
+          <h4 className="about__description-title">Дипломный проект включал 5 этапов</h4>
+          <p className="about__description">Составление плана, работу над бэкендом, вёрстку, добавление функциональности
+            и&nbsp;финальные доработки.</p>
+        </div>
+        <div className="about__description-column">
+          <h4 className="about__description-title">На выполнение диплома ушло 5 недель</h4>
+          <p className="about__description">У&nbsp;каждого этапа был мягкий и&nbsp;жёсткий дедлайн, которые нужно было
+            соблюдать, чтобы успешно защититься.</p>
+        </div>
+
+
       </div>
       <div className="about__grid-container about__grid-container_weeks">
         <h4 className="about__week-count about__week-count_green">1 неделя</h4>
